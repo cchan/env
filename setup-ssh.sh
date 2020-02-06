be565c5cdb3d54629b2497eca62ee27cb6b63b37
Clive Chan <cc@clive.io>
2020-02-06 16:01:21 -0500

    oops ssh needs local user

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
-apt -y install openssh-server
-service sshd start
+sudo apt -y install openssh-server
+sudo service sshd start
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcn0Fqr2VEpUDgak6ZfXI+NMDBzNdEVMpnAbkpuLOGWPpjk5r/67CgSIVVB08Kegc4HtCg4oYqCysmJv8A0pgrZf1gKjcWP51zwldsEOriLEvwXZfwyKoG7wtFRauT+GxyOZt5x02MqerMkWlhoMnXXA/iD9HWrLnqsqIblkygZ7Ifz4nuUA5Kn9FoHWKbqUVv7hTkW9UvuQreYjhgE8nwVBSz13hEIYyqmLhbEuGmGcMNXs/2/1xLr8WipsiXdwOxrfXeuye1tuAw+1Boi6jMGU0dymVqKzsNevXjbi61Lksm6KZWzs2Sh44LAlbEjQ9p3KU85sdUEHVvOEQse5Ol" >> ~/.ssh/authorized_keys
 # There's gotta be a better way to do key management, right?
 
