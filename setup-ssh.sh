195491a8c83c1c30d96b4b3351b50a530c38882a
Clive Chan <cc@clive.io>
2020-02-06 15:03:09 -0500

    install sshd, output the keyfile fingerprint

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+sudo apt -y install openssh-server
+sudo service sshd start
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcn0Fqr2VEpUDgak6ZfXI+NMDBzNdEVMpnAbkpuLOGWPpjk5r/67CgSIVVB08Kegc4HtCg4oYqCysmJv8A0pgrZf1gKjcWP51zwldsEOriLEvwXZfwyKoG7wtFRauT+GxyOZt5x02MqerMkWlhoMnXXA/iD9HWrLnqsqIblkygZ7Ifz4nuUA5Kn9FoHWKbqUVv7hTkW9UvuQreYjhgE8nwVBSz13hEIYyqmLhbEuGmGcMNXs/2/1xLr8WipsiXdwOxrfXeuye1tuAw+1Boi6jMGU0dymVqKzsNevXjbi61Lksm6KZWzs2Sh44LAlbEjQ9p3KU85sdUEHVvOEQse5Ol" >> ~/.ssh/authorized_keys
 # There's gotta be a better way to do key management, right?
+
+for keyfile in /etc/ssh/ssh_host_*_key; do
+  ssh-keygen -lf $keyfile
+done
