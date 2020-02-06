0127f46adebddfb7f035055a6a91b64a106419af
Clive Chan <cc@clive.io>
2020-02-06 15:46:28 -0500

    don't be too crazy for nvidia setup

--- setup-nvidia.sh
+++ setup-nvidia.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+apt-get purge nvidia*
 add-apt-repository ppa:graphics-drivers/ppa
 apt update
 apt install nvidia-driver-440
-service gdm3 stop
-echo 0 > /sys/class/vtconsole/vtcon1/bind
-rmmod nouveau
-modprobe nvidia
-service gdm3 start
+echo "Rebooting now. Try lsmod | grep nvidia to check after the reboot."
+reboot
+#service gdm3 stop
+#echo 0 > /sys/class/vtconsole/vtcon1/bind
+#rmmod nouveau
+#modprobe nvidia
+#service gdm3 start
