59b99c5b980e8fca822ce5ce5541bda0214272c6
Clive Chan <cc@clive.io>
2020-02-19 21:19:11 -0500

    nvm no fancy nvidia install instructions

--- setup-nvidia.sh
+++ setup-nvidia.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-apt-get purge nvidia*
-add-apt-repository ppa:graphics-drivers/ppa
-apt update
-apt install nvidia-driver-440
-echo "Rebooting now. Try lsmod | grep nvidia to check after the reboot."
-reboot
+
+echo Just use https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
+
+#[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+#apt-get purge nvidia*
+#add-apt-repository ppa:graphics-drivers/ppa
+#apt update
+#apt install nvidia-driver-440
+#echo "Rebooting now. Try lsmod | grep nvidia to check after the reboot."
+#reboot
+
 #service gdm3 stop
 #echo 0 > /sys/class/vtconsole/vtcon1/bind
 #rmmod nouveau
