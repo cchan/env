318166838815c2a6d313ef29e11e8f6417419b1a
Clive Chan <cc@clive.io>
2020-02-21 14:43:12 -0500

    now this is finally the right way to do driver installation - 18.04 is so much easier lol

--- setup-nvidia.sh
+++ setup-nvidia.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-driver-440 # nvidia drivers are backwards compatible with older cudatoolkits.
 conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+
+echo "Now run these to test the installation:"
+echo "sudo reboot"
+echo "nvidia-smi"
+echo "python -c 'import torch;print(torch.cuda.is_available())'"
 
 #echo Just use https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
 #echo Also make sure to get CuDNN.
